aaf22e29ede08acd8f2cc28c4757f1d14d9d47bf
Fernando M. Oliveira <koynonia@gmail.com>
2018-08-28 16:22:15 -0300

    Validação sintática com Javax Validation

--- src/main/java/com/linoox/cursomc/services/CategoriaService.java
+++ src/main/java/com/linoox/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.linoox.cursomc.domain.Categoria;
+import com.linoox.cursomc.dto.CategoriaDTO;
 import com.linoox.cursomc.repositories.CategoriaRepository;
 import com.linoox.cursomc.services.exceptions.DataIntegrityException;
 import com.linoox.cursomc.services.exceptions.ObjectNotFoundException;
@@ -57,5 +58,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
