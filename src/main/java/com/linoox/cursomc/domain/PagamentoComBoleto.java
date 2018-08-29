5ac2b6760f292d2da1de415acd07c39f5fc7c2c3
Fernando M. Oliveira <koynonia@gmail.com>
2018-08-29 16:52:08 -0300

    Inserindo Pedido

--- src/main/java/com/linoox/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/linoox/cursomc/domain/PagamentoComBoleto.java
@@ -5,24 +5,27 @@ import java.util.Date;
 import javax.persistence.Entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.linoox.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComBoleto")
 public class PagamentoComBoleto extends Pagamento {
 	private static final long serialVersionUID = 1L;
 
-	@JsonFormat(pattern="dd/MM/yyyy")
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private Date dataVencimento;
-	
-	@JsonFormat(pattern="dd/MM/yyyy")
+
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private Date dataPagamento;
-	
+
 	public PagamentoComBoleto() {
 	}
 
-	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido, Date dataVencimento, Date dataPagamento) {
+	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido, Date dataVencimento,
+			Date dataPagamento) {
 		super(id, estado, pedido);
-	
+
 		this.dataPagamento = dataPagamento;
 		this.dataVencimento = dataVencimento;
 	}
@@ -42,6 +45,4 @@ public class PagamentoComBoleto extends Pagamento {
 	public void setDataPagamento(Date dataPagamento) {
 		this.dataPagamento = dataPagamento;
 	}
-	
-	
 }
--- src/main/java/com/linoox/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/linoox/cursomc/domain/PagamentoComCartao.java
@@ -2,20 +2,22 @@ package com.linoox.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.linoox.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 
 	private Integer numeroDeParcelas;
-	
+
 	public PagamentoComCartao() {
 	}
 
 	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroDeParcelas) {
 		super(id, estado, pedido);
-		
+
 		this.numeroDeParcelas = numeroDeParcelas;
 	}
 
@@ -26,6 +28,4 @@ public class PagamentoComCartao extends Pagamento {
 	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
 		this.numeroDeParcelas = numeroDeParcelas;
 	}
-	
-	
 }
