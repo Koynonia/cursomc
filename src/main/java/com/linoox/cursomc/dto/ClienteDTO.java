ac3bbffccff5db9e539089aa1f5050ee0f0dfa07
Fernando M. Oliveira <koynonia@gmail.com>
2018-08-29 11:13:26 -0300

    Validação costumizada: email não repetido na atualização de Cliente

--- src/main/java/com/linoox/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/linoox/cursomc/dto/ClienteDTO.java
@@ -8,23 +8,25 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.linoox.cursomc.domain.Cliente;
+import com.linoox.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
-	
-	@NotEmpty(message="Preenchimento obrigatório!")
-	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres!")
+
+	@NotEmpty(message = "Preenchimento obrigatório!")
+	@Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres!")
 	private String nome;
-	
-	@NotEmpty(message="Preenchimento obrigatório!")
-	@Email(message="Email inválido!")
+
+	@NotEmpty(message = "Preenchimento obrigatório!")
+	@Email(message = "Email inválido!")
 	private String email;
-	
+
 	public ClienteDTO() {
 	}
-	
+
 	public ClienteDTO(Cliente obj) {
 		id = obj.getId();
 		nome = obj.getNome();
